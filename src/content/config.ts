8fc79c64edea16ac0b10f539798685371f23cfbb
Logan Bunch <loganbunch@gmail.com>
2025-02-02 19:48:48 -0800

    Sun Feb  2 19:48:48 PST 2025

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,22 @@
 import { defineCollection, z } from "astro:content";
-import { file, glob } from "astro/loaders"; // Not available with legacy API
+import { glob } from "astro/loaders"; // Not available with legacy API
 
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/content/blog" }),
   schema: z.object({
     title: z.string(),
+    description: z.string().optional(),
     pubDate: z
       .string()
       .or(z.date())
       .transform((val) => new Date(val)),
+    updatedDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val))
+      .optional(),
+    tags: z.array(z.string()).optional(),
+    draft: z.boolean(),
   }),
 });
 
